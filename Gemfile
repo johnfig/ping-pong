8d2d1bed0c5b181ee11fe508bfca15b3798c414e
John Figueiredo <john@johnfigueiredo.com>
2014-01-19 23:17:48 -0800

    User model up and running with paperclip

---
+++ Gemfile
@@ -0,0 +1,32 @@
+source 'https://rubygems.org'
+ruby "2.1.0"
+gem 'rails', '4.0.2'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem "paperclip", "~> 3.5.2"
+gem "pg", "~> 0.17.1"
+gem 'sqlite3'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :test do
+	gem 'debugger'
+end
+
+# Use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.1.2'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Use Capistrano for deployment
+# gem 'capistrano', group: :development
+
+# Use debugger
+# gem 'debugger', group: [:development, :test]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PingPong::Application.config.secret_key_base = '577a4042f2334ce457a8a451b19d8e5c470482b8b4f768ee0914b35889bc119947d5dc0a90b9db4ac46e04905b9c3a2092241d76153695eb42a63e7a2f67ea1f'
