82d04d002a0b9b589ced85adaf628bc85477111d
John Figueiredo <john@johnfigueiredo.com>
2014-01-20 16:30:35 -0800

    Finished first iteration of ranking algorithm This utilizes weighted averages based on previous rankings

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,29 +9,37 @@ class Game < ActiveRecord::Base
 		
 		calculate_expected_outcome(@loser, @winner)
 
-		@winner.score += 10
+		puts "Updated_scores weighted_average: #{@weighted_average}"
+		@winner.score += 10 * @weighted_average
 		@winner.save
-		@loser.score -= 5 
+		@loser.score -= 5 * @weighted_average
 		@loser.save
 
 		update_rankings
 	end
 
 	def calculate_expected_outcome(loser, winner)
-		# if winner.ranking < loser.ranking 
-		# 	@expected_winner = winner 
-		# 	@expected_loser = loser
-		# 	calculate_weighted_average(@expected_loser, @expected_winner)
-		# elsif winner.ranking > loser.ranking 
-		#   @expected_winner = loser
-		# 	@expected_loser = winner
-		# 	calculate_weighted_average(@expected_loser, @expected_winner)
-		# end
+		if winner.ranking < loser.ranking 
+			@expected_winner = winner 
+			@expected_loser = loser
+			calculate_weighted_average(@expected_loser, @expected_winner, true)
+		elsif winner.ranking > loser.ranking 
+		  @expected_winner = loser
+			@expected_loser = winner
+			calculate_weighted_average(@expected_loser, @expected_winner, false)
+		end
 	end
 
-	def calculate_weighted_average(expected_loser, expected_winner)
-		# @expected_winner_weighted_average =  expected_loser
-		# @expected_loser_weighted_average = 
+	def calculate_weighted_average(expected_loser, expected_winner, predicted_outcome)
+		user_count = User.all.count
+
+		if predicted_outcome == true
+			@weighted_average = 1 * ((expected_winner.ranking - expected_loser.ranking).to_f.abs  / user_count).to_f
+			return @weighted_average
+		else
+			@weighted_average = 1 + ((expected_winner.ranking - expected_loser.ranking).to_f.abs / user_count.to_f)
+			return @weighted_average
+		end
 	end
 
 	def update_rankings
