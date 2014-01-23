fc794270103643db08df4111d5c248ccb8700d7c
John Figueiredo <john@johnfigueiredo.com>
2014-01-23 01:14:55 -0800

    Made javascript cleaner

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
 	$(".new_game").hide();
 	$('#new_user_button').click(function(){
 	    $(".new_user").toggle();
+	    $(".new_game").hide();
 	    return false;
 	 });
 
 	$('#new_game_button').click(function(){
 	    $(".new_game").toggle();
+	    $(".new_user").hide();
 	    return false;
 	 });
 });
