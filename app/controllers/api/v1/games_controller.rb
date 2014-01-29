f3ef7b9b45aaf2a229d160e032ca2a2189a0b667
John Figueiredo <john@johnfigueiredo.com>
2014-01-28 16:33:38 -0800

    finished games api Successfully working locally

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,13 +1,16 @@
 class Api::V1::GamesController < ApplicationController
 def create
-    @game = Game.new
+		params[:winner_id] = params[:winner_id].to_i
+		params[:loser_id] = params[:loser_id].to_i
+
+    @game = Game.new(winner_id: params[:winner_id], loser_id: params[:loser_id])
     loser = User.find(params[:loser_id])
     winner = User.find(params[:winner_id])
     @game.update(winner_ranking: winner.ranking, loser_ranking: loser.ranking)
 
     if @game.save
-    	failure_hash =  { success: 'success', message: 'Congrats, your game was successfully logged!' }
-      render json: leaderboard_array
+    	success_hash =  { success: 'success', message: 'Congrats, your game was successfully logged!' }
+      render json: success_hash
     else
     	failure_hash =  { failure: 'failure', message: 'Oh no. Something happened and this game was not saved!' }
       render json: failure_hash
--- app/controllers/api/v1/leaderboard_controller.rb
+++ app/controllers/api/v1/leaderboard_controller.rb
@@ -16,8 +16,8 @@ class Api::V1::LeaderboardController < ApplicationController
   end
 
   private
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :username, :password, :password_confirmation, :ranking, :avatar)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :password, :password_confirmation, :ranking, :avatar)
+  end
 end
